fd36226e7e2da0fc2d4178822fa0db21d1ff60f5
vivek <stephengmre@gmail.com>
2023-07-06 14:59:15 +0530

    addedsome styles

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,90 +1,90 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/Header.css'
-import { Menu, Search } from '@mui/icons-material'
-import { Link } from 'react-router-dom'
-import { ShoppingCart } from '@mui/icons-material'
-import { useStateValue } from '../StateProvider'
+import React, { useEffect, useState } from 'react';
+import '../styles/Header.css';
+import { Menu, Search } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from '@mui/icons-material';
+import { useStateValue } from '../StateProvider';
 const Header = () => {
-  const { state, dispatch } = useStateValue()
-  const { basket, user } = state
+  const { state, dispatch } = useStateValue();
+  const { basket, user } = state;
   // console.log(state, dispatch)
-  const [active, setActive] = useState(true)
+  const [active, setActive] = useState(true);
   const signOut = () => {
-    localStorage.removeItem('email')
-    localStorage.removeItem('password')
-    localStorage.removeItem('basket')
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+    localStorage.removeItem('basket');
     dispatch({
       type: 'SET_USER',
       user: null,
-    })
-  }
+    });
+  };
   useEffect(() => {
     const id = setInterval(() => {
       if (window.innerWidth >= 350) {
-        setActive(true)
+        setActive(true);
       } else {
-        setActive(false)
+        setActive(false);
       }
-    }, 100)
+    }, 100);
 
     return () => {
-      clearInterval(id)
-    }
-  }, [])
+      clearInterval(id);
+    };
+  }, []);
 
   return (
-    <nav className="header">
-      <Link to="/">
+    <nav className='header'>
+      <Link to='/'>
         <img
-          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-          alt="logo"
-          className="header_logo"
+          src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
+          alt='logo'
+          className='header_logo'
         />
       </Link>
-      <div className="header_search">
-        <input type="text" className="header_input" />
-        <Search className="header_searchIcon" />
+      <div className='header_search'>
+        <input type='text' className='header_input' />
+        <Search className='header_searchIcon' />
       </div>
-      <div className="header_nav">
+      <div className='header_nav'>
         {active && (
-          <div className="nav_link_container">
-            <Link to={!user && '/login'} className="header_link">
-              <div onClick={signOut} className="header_option">
-                <span className="header_option_1">
+          <div className='nav_link_container'>
+            <Link to={!user && '/login'} className='header_link'>
+              <div onClick={signOut} className='header_option'>
+                <span className='header_option_1'>
                   Hello <strong>{user}</strong>
                 </span>
-                <span className="header_option_2">
+                <span className='header_option_2'>
                   {!user ? 'Sign in' : 'Sign out'}
                 </span>
               </div>
             </Link>
-            <Link to="/" className="header_link">
-              <div className="header_option">
-                <span className="header_option_1">Returns</span>
-                <span className="header_option_2">& Orders</span>
+            <Link to='/' className='header_link'>
+              <div className='header_option'>
+                <span className='header_option_1'>Returns</span>
+                <span className='header_option_2'>& Orders</span>
               </div>
             </Link>
-            <Link to="/" className="header_link">
-              <div className="header_option">
-                <span className="header_option_1">Your</span>
-                <span className="header_option_2">Prime</span>
+            <Link to='/' className='header_link'>
+              <div className='header_option'>
+                <span className='header_option_1'>Your</span>
+                <span className='header_option_2'>Prime</span>
               </div>
             </Link>
           </div>
         )}
-        <Link to="/checkout" className="basket_link">
-          <div className="header_option_basket">
-            <ShoppingCart className="header_basket" />
-            <span className="header_basket_count">
+        <Link to='/checkout' className='basket_link'>
+          <div className='header_option_basket'>
+            <ShoppingCart className='header_basket' />
+            <span className='header_basket_count'>
               {basket ? basket.length : 0}
             </span>
           </div>
         </Link>
 
-        {!active && <Menu className="navbar_listIcon" />}
+        {!active && <Menu className='navbar_listIcon' />}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,90 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/Home.css'
-import Products from './Products'
-import { Carousel } from 'react-bootstrap'
-import { usePagination } from '../PaginationContext'
-import Pagination from './Pagination'
+import React, { useEffect, useState } from 'react';
+import '../styles/Home.css';
+import Products from './Products';
+import { Carousel } from 'react-bootstrap';
+import { usePagination } from '../PaginationContext';
+import Pagination from './Pagination';
 
 const Home = () => {
-  const [data, setData] = useState([])
-  const { firstItemIndex, lastItemIndex } = usePagination()
-  const currentItems = data.slice(firstItemIndex, lastItemIndex)
-  const [selectedCategory, setSelectedCategory] = useState('')
-  const [index, setIndex] = useState(0)
-
+  const [data, setData] = useState([]);
+  const { firstItemIndex, lastItemIndex } = usePagination();
+  const currentItems = data.slice(firstItemIndex, lastItemIndex);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [index, setIndex] = useState(0);
+  const [activeCategory, setActiveCategory] = useState(null);
   const filteredItems = selectedCategory
     ? data.filter((item) => item.category === selectedCategory)
-    : currentItems
+    : currentItems;
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
       .then((res) => res.json())
       .then((data) => {
-        setData(data)
+        setData(data);
       })
-      .catch((err) => console.log(err))
-  }, [])
-  const handleCategoryChange = (e) => {
-    const value = e.target.value
-    setSelectedCategory(value === 'All Categories' ? '' : value)
-  }
+      .catch((err) => console.log(err));
+  }, []);
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category === 'All Categories' ? '' : category);
+    setActiveCategory(category);
+  };
+  const handleCategoryClick = (e) => {
+    const value = e.target.value;
+    setSelectedCategory(value === 'All Categories' ? '' : value);
+  };
   //Get all unique categories
-  const categories = [...new Set(data.map((item) => item.category))]
+  const categories = [...new Set(data.map((item) => item.category))];
 
   return (
-    <div className="home">
-      <div className="home_navbar">
-        <select value={selectedCategory} onChange={handleCategoryChange}>
+    <div className='home'>
+      <div className='home_navbar'>
+        <select value={selectedCategory} onChange={handleCategoryClick}>
           <option>All Categories</option>
           {categories.map((category, index) => (
             <option key={index} value={category}>
-              {category}
+              {category.charAt(0).toUpperCase() + category.slice(1)}
             </option>
           ))}
         </select>
-        <span>Sell</span>
-        <span>Amazon miniTV</span>
-        <span>Buy Again</span>
-        <span>Gift Cards</span>
-        <span>Sports,Fitness & Oudoors</span>
-        <span>Baby</span>
+        {categories.map((category, index) => (
+          <span
+            className={activeCategory === category ? 'active' : ''}
+            key={index}
+            value={category}
+            onClick={() => handleCategoryChange(category)}
+          >
+            {category.charAt(0).toUpperCase() + category.slice(1)}
+          </span>
+        ))}
       </div>
 
-      <div className="home_image">
+      <div className='home_image'>
         <Carousel activeIndex={index} onSelect={setIndex}>
           <Carousel.Item>
             <img
-              className="d-block w-100 "
-              src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/Multititle/jan/Blockbuster_entertainment/EN/1500x600_Hero-Tall_01_FT._CB662389308_.jpg"
-              alt="slide1"
+              className='d-block w-100 '
+              src='https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/Multititle/jan/Blockbuster_entertainment/EN/1500x600_Hero-Tall_01_FT._CB662389308_.jpg'
+              alt='slide1'
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 "
-              src="https://images-eu.ssl-images-amazon.com/images/G/31/VG-2019Dec/Desktop_Tallhero_1500-X-600-BB-jan-rvised-NO-Cashback._CB411961897_.jpg"
-              alt="slide2"
+              className='d-block w-100 '
+              src='https://images-eu.ssl-images-amazon.com/images/G/31/VG-2019Dec/Desktop_Tallhero_1500-X-600-BB-jan-rvised-NO-Cashback._CB411961897_.jpg'
+              alt='slide2'
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 "
-              src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Audio/Boat/Boat_Gw_1500x600._CB660810557_.jpg"
-              alt="slide3"
+              className='d-block w-100 '
+              src='https://images-eu.ssl-images-amazon.com/images/G/31/img21/Audio/Boat/Boat_Gw_1500x600._CB660810557_.jpg'
+              alt='slide3'
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 "
-              src="https://images-eu.ssl-images-amazon.com/images/G/31/img2020/fashion/CAT_ATF/2._CB411390334_.jpg"
-              alt="slide4"
+              className='d-block w-100 '
+              src='https://images-eu.ssl-images-amazon.com/images/G/31/img2020/fashion/CAT_ATF/2._CB411390334_.jpg'
+              alt='slide4'
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 "
-              src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/TVs/BAU/catlevel/D20458164_IN_HETV_Category_level_GW_PC_Tollhero_1500x600_en_1._CB660812078_.jpg"
-              alt="slide5"
+              className='d-block w-100 '
+              src='https://images-eu.ssl-images-amazon.com/images/G/31/img20/TVs/BAU/catlevel/D20458164_IN_HETV_Category_level_GW_PC_Tollhero_1500x600_en_1._CB660812078_.jpg'
+              alt='slide5'
             />
           </Carousel.Item>
         </Carousel>
@@ -110,9 +118,9 @@ const Home = () => {
         </button> */}
       </div>
 
-      <div className="home_row">
+      <div className='home_row'>
         {filteredItems.map((item, index) => (
-          <div key={index} className="product_container">
+          <div key={index} className='product_container'>
             <Products
               key={index}
               id={item.id}
@@ -132,7 +140,7 @@ const Home = () => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
